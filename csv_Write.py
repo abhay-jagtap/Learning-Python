63c12b24ba76f854939785af79d44a4cfa1000b9
abhay-jagtap <abhay.djagtap@gmail.com>
2019-06-26 16:55:12 +0530

    Adding new file having program for writing the csv file

---
+++ csv_Write.py
@@ -0,0 +1,24 @@
+import csv, os, sys
+
+x = True
+if not os.path.isfile('Employee.csv'):
+    x = False
+    print('File does not exists, creating new file')
+
+try:
+    with open('Employee.csv','a',newline='') as f:
+        w =csv.writer(f)
+    
+        if not x:
+            w.writerow(['ENo','EName','ESal','EAddr'])
+
+        enum = input('Enter the no of emps: ')
+        for e in range(int(enum)):
+            eno = input('Enter emp no: ')
+            enm = input('Enter emp name: ')
+            esal = input('Enter emp salary: ')
+            eadd = input('Enter emp address: ')
+            w.writerow([eno,enm,esal,eadd])
+    print('csv file successfully created')
+except PermissionError as msg:
+    print('Error: Please close the file from other apps before editing')
